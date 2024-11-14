7d0515b41059377048d2906451549f870340d3e9
Sriharini007 <srihariniinturi@gmail.com>
2024-11-14 22:39:19 +0530

    CountNotebooks.java

---
+++ visaprep/CountNotebooks.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner s = new Scanner(System.in);
+        int x = s.nextInt();
+        int k = x*1000;
+        int n = k/100;
+        System.out.println(n);
+    }
+}
