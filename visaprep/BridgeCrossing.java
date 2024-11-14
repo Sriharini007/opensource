f5814cab35abb201840b718fdf36904d97308921
Sriharini007 <srihariniinturi@gmail.com>
2024-11-14 22:36:08 +0530

    BridgeCrossing.java

---
+++ visaprep/BridgeCrossing.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner s = new Scanner(System.in);
+        int x = s.nextInt();
+        int y = s.nextInt();
+        int z = s.nextInt();
+        int n = z-y;
+        System.out.println(n/x);
+    }
+}
